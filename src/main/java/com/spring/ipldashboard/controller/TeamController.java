7b9cf85412c15b262d706c0a4d842e4b31c7f190
Daniel <danielthomas122@gmail.com>
2021-10-16 14:16:32 -0500

    initial development dashboard

---
+++ src/main/java/com/spring/ipldashboard/controller/TeamController.java
@@ -0,0 +1,33 @@
+package com.spring.ipldashboard.controller;
+
+import com.spring.ipldashboard.controller.repository.MatchRepository;
+import com.spring.ipldashboard.controller.repository.TeamRepository;
+import com.spring.ipldashboard.model.Team;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
+
+        return team;
+        
+    }
+
+
+
+}
